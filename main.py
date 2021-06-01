9eae7553944a1bed444b0e9ededa5162154cfe45
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-06-01 23:26:26 +0200

    fianl_code

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+import svl 
+import time
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Error, too many parameters.")
+        print("Usage : python3 {} <app.exe>".format(sys.argv[0]))
+        exit()
+    else:
+        while True:
+            if svl.process_is_there(sys.argv[1]):
+                proc_found = svl.research_process_by_name(sys.argv[1])
+                if proc_found:
+                    svl.run(proc_found)
+            else:
+                time.sleep(10)
+                
+
+           
+ 
---
+++ svl.py
@@ -0,0 +1,41 @@
+import psutil
+import time
+
+def find_and_kill_proc(proc):
+    """ Process -> bool
+    Kill les processus de même nom et renvoie True si au moins une instance a été kill, False sinon."""
+    killed = False
+    for process in psutil.process_iter(['name']):
+        if process == proc :
+            continue
+        if process.info['name'] == proc.name():
+            process.kill()
+            if not killed:
+                killed = True
+    return killed
+
+def research_process_by_name(name):
+    """ str -> Process
+    Retourne le processus 'name'."""
+    for process in psutil.process_iter():
+        if process.name() == name:
+            return process
+
+def process_is_there(name):
+    """ str -> bool
+    Renvoie True si le process name est présent, False sinon."""
+    return name in psutil.process_iter(['name'])
+
+def run(process):
+    """ Process ->
+    Tant que le processus tourne, on recherche d'autre instance du même nom."""
+    while process and process.is_running():
+        if not find_and_kill_proc(process):
+            time.sleep(10)
+        
+
+
+
+    
+
+
