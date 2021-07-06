641746f716ebe384d43b20987744ff4532bf9b8a
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2021-07-06 21:42:02 +0200

    final_code

--- svl.py
+++ svl.py
@@ -1,9 +1,10 @@
 import psutil
 import time 
+import math
 
 def find_and_kill_proc(proc):
     """ Process -> bool
-    Kill les processus de même nom et renvoie True si au moins une instance a été kill, False sinon."""
+    Kills processes with the same name and returns True if at least one instance was killed, False otherwise."""
     killed = False
     for process in psutil.process_iter(['name']):
         if process == proc :
@@ -16,13 +17,13 @@ def find_and_kill_proc(proc):
 
 def research_process_by_name(name):
     """ str -> Process
-    Retourne le processus 'name'."""
-    started = 0
+    Returns the process 'name'."""
+    started = math.inf
     process = None
     for p in psutil.process_iter():
         try:
             if p.name() == name:
-                if p.create_time() > started:
+                if p.create_time() < started:
                     started = p.create_time()
                     process = psutil.Process(p.pid)
         except (psutil.AccessDenied, psutil.NoSuchProcess):
@@ -33,7 +34,7 @@ def research_process_by_name(name):
 
 def run(process):
     """ Process ->
-    Tant que le processus tourne, on recherche d'autre instance du même nom."""
+    As long as the process is running, we are looking for another instance of the same name."""
     while (process and process.is_running()) or (process.status() == psutil.STATUS_RUNNING):
         if not find_and_kill_proc(process):
             time.sleep(10)
